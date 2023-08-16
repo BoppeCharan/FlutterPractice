538a2976db4e6e55d432222bfe53ed000abc0e5f
BoppeCharan <boppesricharan@gmail.com>
2023-08-17 00:03:23 +0530

    Added SnackBar

--- lib/screens/page_one.dart
+++ lib/screens/page_one.dart
@@ -15,11 +15,22 @@ class Page_One extends StatelessWidget {
         leading: const Icon(Icons.menu_book),
       ),
       body: MyBodyText(),
-      floatingActionButton: const FloatingActionButton(
-        onPressed: null,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => {_showSnackBar(context)},
         tooltip: 'Love',
         hoverColor: Colors.amber,
         child: Icon(Icons.favorite_sharp),
+      ),
+    );
+  }
+
+  _showSnackBar(BuildContext context) {
+    final scaffold = ScaffoldMessenger.of(context);
+    scaffold.showSnackBar(
+      SnackBar(
+        content: const Text('Added to favorite'),
+        action: SnackBarAction(
+            label: 'UNDO', onPressed: scaffold.hideCurrentSnackBar),
       ),
     );
   }
@@ -32,17 +43,17 @@ class MyBodyText extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.fromLTRB(10,20,10,0),
+      margin: const EdgeInsets.fromLTRB(10, 20, 10, 0),
       child: Column(
-
         children: [
           TextField(
-            controller:  controller,
+            controller: controller,
             keyboardType: TextInputType.name,
             decoration: InputDecoration(
                 label: const Text("Name"),
-                hintText :"Enter Name!!!",
-                hintStyle: const TextStyle(color: Color.fromARGB(255, 169, 238, 79)),
+                hintText: "Enter Name!!!",
+                hintStyle:
+                    const TextStyle(color: Color.fromARGB(255, 169, 238, 79)),
                 contentPadding: const EdgeInsets.all(20.0),
                 prefixIcon: const Icon(Icons.people),
                 border: OutlineInputBorder(
@@ -53,11 +64,12 @@ class MyBodyText extends StatelessWidget {
                         color: Colors.amberAccent))),
           ),
           const SizedBox(height: 10),
-          ElevatedButton(onPressed: () {
-            print("Entered Name is : "+ controller.text);
-            controller.clear();
-          }, 
-          child: const Text("Get Name"),
+          ElevatedButton(
+            onPressed: () {
+              print("Entered Name is : " + controller.text);
+              controller.clear();
+            },
+            child: const Text("Get Name"),
           )
         ],
       ),
